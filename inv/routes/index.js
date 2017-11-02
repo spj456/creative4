bbbf9197b08717b8c895da1a9d8a5c81457239f4
spj456 <spj432@gmail.com>
2017-11-02 16:49:43 -0600

    added more commands

--- inv/routes/index.js
+++ inv/routes/index.js
@@ -7,19 +7,24 @@ router.get('/', function (req, res) {
     res.sendFile('index.html', { root: 'public' });
 })
 
-router.get('/addItem', function(req, res) {
+router.get('/addItem', function (req, res) {
     console.log("adds item");
-    fs.appendFile('message.txt', 'data to append', function (err) {
+    fs.appendFile('items.dat.txt', req.query.q, function (err) {
         if (err) throw err;
         console.log('Saved!');
     });
 })
 
-router.get('/deleteItem', function(req, res) {
-
+router.get('/addItemQuantity', function (req, res) {
+    console.log("adds item");
+    fs.appendFile('quantity.dat.txt', req.query.q, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
 })
 
-router.get('/getItem', function (req, res) {
+router.get('/changeQuantiy', function (req, res) {
+    console.log("changes quantity");
     fs.readFile(__dirname + '/items.dat.txt', function (err, data) {
         if (err) throw err;
         fs.readFile(__dirname + '/quantity.dat.txt', function (err, data2) {
@@ -43,33 +48,55 @@ router.get('/getItem', function (req, res) {
 );
 })
 
-router.get('/getword', function (req, res, next) {
-    console.log("gets a word");
-    var myword = "https://owlbot.info/api/v1/dictionary/";
-    myword += req.query.q;
-    request(myword).pipe(res);
+router.get('/deleteItem', function (req, res) {
+    console.log("deletes item");
+    fs.readFile(__dirname + '/items.dat.txt', function (err, data) {
+        if (err) throw err;
+        fs.readFile(__dirname + '/quantity.dat.txt', function (err, data2) {
+            if (err) throw err;
+            var quantities = data2.toString().split("\n");
+            var items = data.toString().split("\n");
+            var myRe = new RegExp("^" + req.query.q);
+            var jsonresult = [];
+            for (var i = 0; i < items.length; i++) {
+                var result = items[i].search(myRe);
+                if (result != -1) {
+                    jsonresult.push({
+                        item: items[i],
+                        quantity: quantities[i]
+                    });
+                }
+            }
+            res.status(200).json(jsonresult);
+        });
+    }
+);
 })
 
-router.get('/getcity', function (req, res, next) {
-    console.log("getCity");
-    fs.readFile(__dirname + '/cities.dat.txt',function(err,data) {
-        if(err) throw err;
-        var cities = data.toString().split("\n");
-        var myRe = new RegExp("^" + req.query.q);
-        var jsonresult = [];
-        for(var i = 0; i < cities.length; i++) {
-            var result = cities[i].search(myRe); 
-            if(result != -1) {
-                jsonresult.push({city:cities[i]});
-            } 
-        }
-        res.status(200).json(jsonresult);
-
+router.get('/getItem', function (req, res) {
+    console.log("gets item");
+    fs.readFile(__dirname + '/items.dat.txt', function (err, data) {
+        if (err) throw err;
+        fs.readFile(__dirname + '/quantity.dat.txt', function (err, data2) {
+            if (err) throw err;
+            var quantities = data2.toString().split("\n");
+            var items = data.toString().split("\n");
+            var myRe = new RegExp("^" + req.query.q);
+            var jsonresult = [];
+            for (var i = 0; i < items.length; i++) {
+                var result = items[i].search(myRe);
+                if (result != -1) {
+                    jsonresult.push({
+                        item: items[i],
+                        quantity: quantities[i]
+                    });
+                }
+            }
+            res.status(200).json(jsonresult);
+        });
     }
-    );
-
-});
-
+);
+})
 
 module.exports = router;
 
