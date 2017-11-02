d5db2eb903ee44b1fdf93120a2145c0777ede9dc
spj456 <spj432@gmail.com>
2017-11-02 17:06:39 -0600

    getting closer

--- inv/routes/index.js
+++ inv/routes/index.js
@@ -29,17 +29,14 @@ router.get('/changeQuantiy', function (req, res) {
         if (err) throw err;
         fs.readFile(__dirname + '/quantity.dat.txt', function (err, data2) {
             if (err) throw err;
+            
             var quantities = data2.toString().split("\n");
             var items = data.toString().split("\n");
-            var myRe = new RegExp("^" + req.query.q);
-            var jsonresult = [];
+            var quantityresult = [];
             for (var i = 0; i < items.length; i++) {
-                var result = items[i].search(myRe);
+                var result = items[i].search(req.query.item);
                 if (result != -1) {
-                    jsonresult.push({
-                        item: items[i],
-                        quantity: quantities[i]
-                    });
+                    items[i] = req.query.quantity;
                 }
             }
             res.status(200).json(jsonresult);
@@ -50,16 +47,16 @@ router.get('/changeQuantiy', function (req, res) {
 
 router.get('/deleteItem', function (req, res) {
     console.log("deletes item");
+    console.log("changes quantity");
     fs.readFile(__dirname + '/items.dat.txt', function (err, data) {
         if (err) throw err;
         fs.readFile(__dirname + '/quantity.dat.txt', function (err, data2) {
             if (err) throw err;
             var quantities = data2.toString().split("\n");
             var items = data.toString().split("\n");
-            var myRe = new RegExp("^" + req.query.q);
             var jsonresult = [];
             for (var i = 0; i < items.length; i++) {
-                var result = items[i].search(myRe);
+                var result = items[i].search(req.query.q);
                 if (result != -1) {
                     jsonresult.push({
                         item: items[i],
