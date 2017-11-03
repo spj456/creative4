2c273c7b347a6525506ca1cf41057979d019af0e
Mike Crowther <macrowther7+git@gmail.com>
2017-11-02 21:41:46 -0600

    another commit

--- inv/routes/index.js
+++ inv/routes/index.js
@@ -4,18 +4,38 @@ var request = require('request')
 var router = express.Router();
 
 router.get('/', function (req, res) {
-    res.sendFile('index.html', { root: 'public' });
+    res.sendFile('index.html', {
+        root: 'public'
+    });
 })
 
-router.post('/addItem', function (req, res) {
+router.get('/addItem', function (req, res) {
     console.log("adds item");
-    fs.appendFile('items.dat.txt', req.query.item, function (err) {
+    fs.appendFile(__dirname + '/items.dat.txt', "\n" + req.query.item, function (err) {
         if (err) throw err;
+        console.log(req.query.item);
         console.log('Saved!');
     });
-    fs.appendFile('quantity.dat.txt', req.query.quantity, function (err) {
+    fs.appendFile(__dirname + "/quantity.dat.txt", "\n" + req.query.quantity, function (err) {
         if (err) throw err;
-        console.log('Saved!');
+        console.log("Saved!");
+    });
+    fs.readFile(__dirname + '/items.dat.txt', function (err, data) {
+        if (err) throw err;
+        fs.readFile(__dirname + '/quantity.dat.txt', function (err, data2) {
+            if (err) throw err;
+            var quantities = data2.toString().split("\n");
+            var items = data.toString().split("\n");
+            var jsonresult = [];
+            for (var i = 0; i < items.length; i++) {
+                jsonresult.push({
+                    item: items[i],
+                    quantity: quantities[i]
+                });
+            }
+            console.log(jsonresult);
+            res.status(200).json(jsonresult);
+        });
     });
 })
 
@@ -25,7 +45,7 @@ router.get('/changeQuantiy', function (req, res) {
         if (err) throw err;
         fs.readFile(__dirname + '/quantity.dat.txt', function (err, data2) {
             if (err) throw err;
-            
+
             var quantities = data2.toString().split("\n");
             var items = data.toString().split("\n");
             var quantityresult = [];
@@ -37,33 +57,44 @@ router.get('/changeQuantiy', function (req, res) {
             }
             res.status(200).json(jsonresult);
         });
-    }
-);
+    });
 })
 
 router.get('/deleteItem', function (req, res) {
     console.log("deletes item");
     console.log("changes quantity");
+    var jsonresult = [];
+    var newItems = [];
+    var newQuantities = [];
     fs.readFile(__dirname + '/items.dat.txt', function (err, data) {
         if (err) throw err;
         fs.readFile(__dirname + '/quantity.dat.txt', function (err, data2) {
             if (err) throw err;
             var quantities = data2.toString().split("\n");
             var items = data.toString().split("\n");
-            var jsonresult = [];
             for (var i = 0; i < items.length; i++) {
-                var result = items[i].search(req.query.quantity);
-                if (result != -1) {
+                // var result = items[i].search(req.query.quantity);
+                if (items[i] != req.query.item) {
                     jsonresult.push({
                         item: items[i],
                         quantity: quantities[i]
                     });
+                    newItems.push(items[i] + "\n");
+                    newQuantities.push(quantities[i] + "\n");
                 }
             }
+            fs.writeFile(__dirname + "/items.dat.txt", newItems, function (err) {
+                if (err) throw err;
+                console.log("Saved!");
+            });
+            fs.writeFile(__dirname + '/quantity.dat.txt', newQuantities, function (err) {
+                if (err) throw err;
+                console.log("Saved!");
+            });
             res.status(200).json(jsonresult);
         });
-    }
-);
+    });
+    
 })
 
 router.get('/getItem', function (req, res) {
@@ -74,22 +105,20 @@ router.get('/getItem', function (req, res) {
             if (err) throw err;
             var quantities = data2.toString().split("\n");
             var items = data.toString().split("\n");
-            var myRe = new RegExp("^" + req.query.quantity);
+            // var myRe = new RegExp("^" + req.query.quantity);
             var jsonresult = [];
             for (var i = 0; i < items.length; i++) {
-                var result = items[i].search(myRe);
-                if (result != -1) {
-                    jsonresult.push({
-                        item: items[i],
-                        quantity: quantities[i]
-                    });
-                }
+                // var result = items[i].search(myRe);
+                // if (result != -1) {
+                jsonresult.push({
+                    item: items[i],
+                    quantity: quantities[i]
+                });
+                // }
             }
             res.status(200).json(jsonresult);
         });
-    }
-);
+    });
 })
 
 module.exports = router;
-
